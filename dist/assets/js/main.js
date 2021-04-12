7b5db4acd4633c906b92620762eda50c3af8108d
Nam Dang Nguyen <namdangnguyen09@gmail.com>
2021-04-12 17:41:09 +0700

    update pgae contact home privary and css

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -4,3 +4,14 @@ function getYear() {
 	    $(this).text(new Date().getFullYear());
     });
 }
+
+// $(document).ready(function () {        
+//     var width = $('.g-recaptcha').parent().width();
+//     if (width < 302) {
+//         var scale = width / 302;
+//         $('.g-recaptcha').css('transform', 'scale(' + scale + ')');
+//         $('.g-recaptcha').css('-webkit-transform', 'scale(' + scale + ')');
+//         $('.g-recaptcha').css('transform-origin', '0 0');
+//         $('.g-recaptcha').css('-webkit-transform-origin', '0 0');
+//     }
+// }); 
