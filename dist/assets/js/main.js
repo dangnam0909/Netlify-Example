7bd1ba6aff7e3ef90b415e84249ef0d69f40acd0
Nam Dang Nguyen <namdangnguyen09@gmail.com>
2021-04-14 17:39:34 +0700

    update pgae contact home privary and css navmenu

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,17 +1,8 @@
-function getYear() {
+$(function () {
     // for add year
     $('.year').each(function () {
 	    $(this).text(new Date().getFullYear());
     });
-}
-
-// $(document).ready(function () {        
-//     var width = $('.g-recaptcha').parent().width();
-//     if (width < 302) {
-//         var scale = width / 302;
-//         $('.g-recaptcha').css('transform', 'scale(' + scale + ')');
-//         $('.g-recaptcha').css('-webkit-transform', 'scale(' + scale + ')');
-//         $('.g-recaptcha').css('transform-origin', '0 0');
-//         $('.g-recaptcha').css('-webkit-transform-origin', '0 0');
-//     }
-// }); 
+    
+    // capcha
+});
