cd1f3980e5c98a9fd7db44502b62a261e2a382e1
NamND <namnd@nitrotech.asia>
2021-04-16 01:46:48 +0700

    update code css and html

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -3,22 +3,7 @@ $(function () {
     $('.year').each(function () {
         $(this).text(new Date().getFullYear());
     });
-
-    // capcha
 });
-
-// active
-var header = document.getElementById("myTopnav");
-var btns = header.getElementsByClassName("menu");
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function () {
-        var current = document.getElementsByClassName("active");
-        if (current.length > 0) {
-            current[0].className = current[0].className.replace(" active", "");
-        }
-        this.className += " active";
-    });
-}
 
 // menu reponsive
 function myFunction() {
